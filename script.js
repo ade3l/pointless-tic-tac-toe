85dd1f4f2e5701395d7fe4736b40ecd1311f921d
Adeel <adeelasakkeer@gmail.com>
2022-09-16 11:45:17 +0530

    validate g ame board done

--- script.js
+++ script.js
@@ -1,15 +1,59 @@
 let gameBoard = (()=>{
+    let _winner
+    let _gameOver  = false
     let _state = new Array(3)
     for(let i = 0; i < _state.length; i++){
         _state[i] = new Array(3).fill(0)
     }
-    console.log(_state);
     let _currPlayer = 0
+    const isGameOver = ()=> _gameOver
     const select = (num)=>{
         // console.log(num);
-        console.log(_state[Math.floor(num/3)][num%3]);
+        if(_currPlayer==0){
+            _state[Math.floor(num/3)][num%3] = 1
+            _currPlayer = 1
+        }
+        else{
+            _state[Math.floor(num/3)][num%3] = -1
+            _currPlayer = 0
+        }
+        validateBoard()
     }
-    return{select}
+    const getChar = ()=>{
+        if(_currPlayer==0) return 'X'
+        else return 'O'
+    }
+
+    const validateBoard = ()=>{
+        //check rows
+        for(let i = 0; i < _state.length; i++){
+            if(_state[i][0] == _state[i][1] && _state[i][1] == _state[i][2] && _state[i][0] != 0){
+                _winner = _state[i][0]==1?0:1
+                console.log(_winner);
+                _gameOver = true
+            }
+        }
+        //check columns
+        for(let i = 0; i < _state.length; i++){
+            if(_state[0][i] == _state[1][i] && _state[1][i] == _state[2][i] && _state[0][i] != 0){
+                _winner = _state[0][i]==1?0:1
+                console.log(_winner);
+                _gameOver = true
+            }
+        }
+        //check diagonals
+        if(_state[0][0] == _state[1][1] && _state[1][1] == _state[2][2] && _state[0][0] != 0){
+            _winner = _state[0][0]==1?0:1
+            console.log(_winner);
+            _gameOver = true
+        }
+        if(_state[0][2] == _state[1][1] && _state[1][1] == _state[2][0] && _state[0][2] != 0){
+            _winner = _state[0][2]==1?0:1
+            console.log(_winner);
+            _gameOver = true
+        }
+    }
+    return{select, getChar, isGameOver}
 })()
 let cells = []
 const cellFactory = (element)=> {
@@ -22,7 +66,8 @@ const cellFactory = (element)=> {
     this.getElement = ()=> _element
     element.addEventListener('click',()=>{
         // console.log(_number);
-        if(_playable){
+        if(_playable && !gameBoard.isGameOver()){
+            _element.textContent = gameBoard.getChar()
             gameBoard.select(_number)
             _playable = false
         }
